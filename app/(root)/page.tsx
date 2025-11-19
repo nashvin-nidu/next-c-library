06097cfd02c45ad0e2f0dc5b2007c7169d87886f
Nashvin Nidu <nashvinnidu1111@gmail.com>
2025-11-19 23:57:28 +0530

    getting data from contant

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,11 @@
 import { ComponentCard } from "@/components/ui/card";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import data_components from "@/lib/constants/data-components";
+
+
 
 export default function Page() {
-
-  const components = [
-    {
-      title: "Button",
-      description: "Magic Ui",
-      component_slug: "button-preview"
-    }
-  ]
-
-
+  const data = data_components;
   return (
     <div className="h-screen flex flex-col ">
       <div>
@@ -20,8 +14,8 @@ export default function Page() {
 
       <div className=" m-3 px-5 bg-(--container-app) border-2 rounded-2xl">
         <div className="overflow-x-auto scrollbar-hide  flex mt-5 gap-7 pb-5">
-          {components.map((comp) => (
-            <ComponentCard key={comp.component_slug} title={comp.title} description={comp.description} video_slug={comp.component_slug} />
+          {data.map((component) => (
+            <ComponentCard key={component.title} data={component} />
           ))}
         </div>
         
