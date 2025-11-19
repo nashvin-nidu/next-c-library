db3fe64c3bec67533148528cf671025caea65371
Nashvin Nidu <nashvinnidu1111@gmail.com>
2025-11-19 23:55:49 +0530

    added constant data

---
+++ lib/constants/data-components.ts
@@ -0,0 +1,23 @@
+
+
+export interface Component {
+  slug: string;
+  title: string;
+  tagline: string;
+}
+
+ const data_components: Component[] = [
+    {
+      slug: "button-preview",
+      tagline: "Magic Ui",
+      title: "Woking",
+    },
+    {
+      slug: "button-preview",
+      tagline: "Magic Ui",
+      title: "Wokingd",
+    }
+  ]
+
+
+export default data_components;
