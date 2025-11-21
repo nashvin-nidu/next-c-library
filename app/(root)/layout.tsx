f33cac837ae81a2eb9d1a83841779d42fe2c73b7
Nashvin Nidu <nashvinnidu1111@gmail.com>
2025-11-21 23:57:32 +0530

    changed file location of layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,9 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
+<<<<<<< Updated upstream
 import { AppSidebar } from "@/components/app/sidebar";
+=======
+import { AppSidebar } from "@/components/sidebar";
+>>>>>>> Stashed changes
 
 
 const HomePageLayout = ({children} : {children : React.ReactNode}) => {
