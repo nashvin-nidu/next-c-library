1f096061895c7605665d646b7cd1608eadd1c7ea
Nashvin Nidu <nashvinnidu1111@gmail.com>
2025-11-23 23:12:57 +0530

    Upadated Buttons to toggle Dark and Light Theme

--- components/page-component/Buttons.tsx
+++ components/page-component/Buttons.tsx
@@ -1,11 +1,26 @@
-import { Copy, FileText, Sun } from "lucide-react"
+'use client'
+import { useTheme } from "next-themes"
+import { Copy, FileText, Moon, Sun } from "lucide-react"
 
 export const ThemeButton = () =>{
+      const { resolvedTheme, setTheme } = useTheme()
+      const toggleTheme = () => {
+        setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+      }
     return(
         <div className="h-15">
-            <button type="button" id="" className="flex flex-center gap-2 py-1 px-3 bg-card border-2 rounded-md cursor-pointer" >
-                <Sun className="w-4 h-4" />
-                <span>Dark</span>
+            <button 
+                type="button" 
+                id="theme" 
+                onClick={toggleTheme}
+                className="flex flex-center gap-2 py-1 px-3 bg-card border-2 rounded-md cursor-pointer" 
+            >
+                {resolvedTheme === 'dark' ? (
+                    <Sun className="w-4 h-4" />
+                ) : (
+                    <Moon className="w-4 h-4" />
+                )}
+                <span>{resolvedTheme === 'dark' ? 'Light' : 'Dark'}</span>
             </button>
         </div>
         
