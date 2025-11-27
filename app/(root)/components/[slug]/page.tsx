3eefee33855f7c372ba8fcfcd6cabf05e8817c00
Nashvin Nidu <nashvinnidu1111@gmail.com>
2025-11-27 10:25:58 +0530

    Breadcrumbs on pages

--- app/(root)/components/[slug]/page.tsx
+++ app/(root)/components/[slug]/page.tsx
@@ -3,18 +3,42 @@ import {ComponentData, componentRegistry} from "@/lib/component-registry";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
 
 export default async function  Components({params} : {params: Promise<{slug:string}>}) {
   const {slug} = await params;
-  console.log(slug)
   const data : ComponentData[] = componentRegistry[slug] || [];
 
 
   return (
     <div className="m-5 h-[95%] p-5 bg-card border-2 rounded-md">
-            <Link href="/">
+      <div className="flex gap-2 items-center">
+        <Link href="/">
               <ArrowLeft width={20} height={20}/>
-            </Link>
+        </Link>
+
+            <Breadcrumb>
+              <BreadcrumbList className="text-lg items-center">
+                <BreadcrumbItem>
+                  <BreadcrumbLink asChild>
+                    <Link href="/">Home</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>{slug.charAt(0).toUpperCase() + slug.slice(1)}</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+      </div>
 
             <div className="flex flex-wrap scrollbar-hide mt-5 pb-5 gap-5 overflow-y-auto  md: wrap-normal">
                 {data.map((component) => (
