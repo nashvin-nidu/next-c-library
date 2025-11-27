38beee06f81d35c83b13cc4e9a04e1a761887d8a
Nashvin Nidu <nashvinnidu1111@gmail.com>
2025-11-27 10:28:08 +0530

    footermenu dependancy updated

--- components/page-component/FooterMenuContent.tsx
+++ components/page-component/FooterMenuContent.tsx
@@ -3,7 +3,7 @@ const FooterMenuContent = ({dataCard} : {dataCard : string[]}) =>{
                 <div className="mb-30">
                     <h1 className="font-sans font-bold mb-3">Dependanceies</h1>
                     {dataCard.map(i => (
-                        <span key={i} className="bg-card broder-2 rounded-xl px-3 py-2">{i}</span>  
+                        <span key={i} className="bg-card broder-2 rounded-xl px-3 py-2 ml-2">{i}</span>  
                     ))}
                 </div>
     )
