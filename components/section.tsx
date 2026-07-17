666011f574a3323cb410bef5674a0eadbde24efa
Nashvin Nidu <99200174+nashvin-nidu@users.noreply.github.com>
2026-07-17 23:58:20 +0530

    Update section.tsx

--- components/section.tsx
+++ components/section.tsx
@@ -10,9 +10,11 @@ export default function Section({data, title, endPoint}: {data: ComponentData[];
         <div className="flex justify-between pt-3 pr-10">
           <h2 className="text-sm">{title}</h2>
           <Link href={`/components${endPoint}`} className="flex flex-center gap-2 ">
+
             <p className="text-sm">View All</p>
             <ArrowRight width={16} height={16}/>
           </Link>
+
         </div>
         
         <div className="overflow-x-auto scrollbar-hide  flex mt-5 gap-7 pb-5">
