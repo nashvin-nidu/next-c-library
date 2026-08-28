3fffe0fc15eed24413cd2221b324cca5a5541e83
Nashvin Nidu <99200174+nashvin-nidu@users.noreply.github.com>
2026-08-28 23:52:24 +0530

    Update section.tsx

--- components/section.tsx
+++ components/section.tsx
@@ -18,12 +18,11 @@ export default function Section({data, title, endPoint}: {data: ComponentData[];
             <p className="text-sm">View All</p>
             <ArrowRight width={16} height={16}/>
           </Link>
-
-
-
         </div>
 
         
+
+
         <div className="overflow-x-auto scrollbar-hide  flex mt-5 gap-7 pb-5">
           {data.map((component: ComponentData) => 
           (
