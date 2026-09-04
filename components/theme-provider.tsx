e593b1465364f89c5637ccb38406e0e41c8a1beb
Nashvin Nidu <99200174+nashvin-nidu@users.noreply.github.com>
2026-09-04 23:46:03 +0530

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 export function ThemeProvider({
 
   children,
+
+
   ...props
 
 }: React.ComponentProps<typeof NextThemesProvider>) {
